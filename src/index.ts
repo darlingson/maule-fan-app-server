f2b3753c37f9f29f3fcdf8064eb451736f74ee9c
darlingson <dmakuwila@gmail.com>
2025-12-06 15:49:21 +0200

    connected to neondb

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-
+import { sql } from '../src/services/db.js';
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.get('/api/teams', async (c) => {
+  console.log("endpoint hit")
+  const teams = await sql`SELECT id, name, short_name FROM teams ORDER BY name`;
+  return c.json(teams);
+});
+
 
 serve({
   fetch: app.fetch,
