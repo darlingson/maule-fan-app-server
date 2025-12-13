945d427587d60e1fb332e3df334ac5512e9c4c14
darlingson <dmakuwila@gmail.com>
2025-12-13 18:55:17 +0200

    added routes to /api/index to allow vercel routing to work

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,13 @@
-import '../src/index';
+import { handle } from 'hono/vercel';
+import { Hono } from 'hono';
+
+import { playerRoutes, competitionRoutes, matchRoutes, teamRoutes } from '../src/router/index.js';
+
+const app = new Hono()
+  .route('/api/players', playerRoutes)
+  .route('/api/competitions', competitionRoutes)
+  .route('/api/matches', matchRoutes)
+  .route('/api/teams', teamRoutes)
+  .get('/', (c) => c.text('Hello Hono!'));
+
+export default handle(app);
